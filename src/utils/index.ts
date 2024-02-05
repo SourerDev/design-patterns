534c1e826a1d1875a234d011ce96fe24edcd98b5
SourerDev <jhonny30ca@gmail.com>
2024-02-05 18:26:20 -0500

    feat

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,9 @@
 import { execSync } from 'child_process'
+const inquirer = import('inquirer')
 
+export async function runInquirer() {
+  return (await inquirer).default.prompt
+}
 export function runCommand(command: string) {
   const output = execSync(command)
   console.log(output.toString())
